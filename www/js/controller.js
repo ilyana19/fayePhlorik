d75a0d712104845698146322092715d67848bdfc
Linda Lou <aries_2152@hotmail.com>
2015-03-27 11:50:53 -0400

    more realistic time

--- www/js/controller.js
+++ www/js/controller.js
@@ -37,15 +37,20 @@ var app = function(app) {
 	app.statDepletion = function(assets, pages) {
 		zog("handle stats");
 		
-		var decreaseStat = new zim.Proportion(0, 120, 0, 1, -1);
-		var decreaseWater = new zim.Proportion(0, 60, 0, 1, -1);
-		var increaseStat = new zim.Proportion(0, 180, 0, 1);
+		var decreaseHP = new zim.Proportion(0, 5, 0, 1, -1);
+		var decreaseFood = new zim.Proportion(0, 120, 0, 1, -1);
+		var decreaseWater = new zim.Proportion(0, 5, 0, 1, -1);
+		var increaseFriendliness = new zim.Proportion(0, 180, 0, 1);
 		
 		var interval = setInterval(function() {
-			assets.hp.scaleX = decreaseStat.convert((Date.now()-window.localStorage.time)/1000);
-			assets.hungerStat.scaleX = decreaseStat.convert((Date.now()-window.localStorage.time)/1000);
-			assets.waterStat.scaleX = decreaseWater.convert((Date.now()-window.localStorage.time)/1000);
-			assets.friendliness.scaleX = increaseStat.convert((Date.now()-window.localStorage.time)/1000);
+			assets.hungerStat.scaleX = decreaseFood.convert((Date.now()-window.localStorage.time)/1000/60);
+			assets.waterStat.scaleX = decreaseWater.convert((Date.now()-window.localStorage.time)/1000/60);
+			assets.friendliness.scaleX = increaseFriendliness.convert((Date.now()-window.localStorage.time)/1000/60);
+			
+			if (assets.hungerStat.scaleX || assets.waterStat.scaleX == 0) {
+				zog("faye's hungry and thirsty. he's dying.");
+				assets.hp.scaleX = decreaseHP.convert((Date.now()-window.localStorage.time)/1000/60);
+			}
 			
 			if (assets.hp.scaleX == 0) {
 				zog("dead");
