c2262034713d5c767693f98426cdb1b023fde612
Linda Lou <aries_2152@hotmail.com>
2015-03-27 11:10:57 -0400

    Added stats

--- www/js/controller.js
+++ www/js/controller.js
@@ -5,29 +5,55 @@ var app = function(app) {
 		zog("hotspots");
 		
 		var buttons = new zim.HotSpots([
-				{page: assets.main, rect: assets.food, call: giveFood},
-				{page: assets.main, rect: assets.water, call: giveWater},
 				{page: assets.main, rect: assets.training, call: train}
 			]
 		);
 		
-		function giveFood() {
-			zog("give faye food");
-			assets.food.setFill("orange");
-			
-			//function(obj, rect, overCursor, dragCursor, currentTarget, mouseDowns, localBounds)
-			
-			
-			stage.update();
-		}
-		
-		function giveWater() {
-			zog("give faye water");
-		}
-		
 		function train() {
 			zog("train faye");
 		}
+	}
+	
+	app.makeDrag = function(assets, pages) {
+		zog("drag stuff");
+		
+		//function(obj, rect, overCursor, dragCursor, currentTarget, mouseDowns, localBounds)
+		/*zim.drag(assets.buttons, null, null, null, null, true);
+
+		assets.buttons.on("mousedown", function(e) {
+			var copy = e.target.clone();
+			copy.x = e.target.x;
+			copy.y = e.target.y;
+			assets.buttons.addChildAt(copy, 0);
+			assets.buttons.addChild(e.target);
+			zog(e.target.x, e.target.y);
+		});*/
+		
+		//zim.drag(assets.buttons);
+		
+		zim.drag(assets.buttons, null, null, null, null, true);
+	}
+	
+	app.statDepletion = function(assets, pages) {
+		zog("handle stats");
+		
+		var decreaseStat = new zim.Proportion(0, 120, 0, 1, -1);
+		var decreaseWater = new zim.Proportion(0, 60, 0, 1, -1);
+		var increaseStat = new zim.Proportion(0, 180, 0, 1);
+		
+		var interval = setInterval(function() {
+			assets.hp.scaleX = decreaseStat.convert((Date.now()-window.localStorage.time)/1000);
+			assets.hungerStat.scaleX = decreaseStat.convert((Date.now()-window.localStorage.time)/1000);
+			assets.waterStat.scaleX = decreaseWater.convert((Date.now()-window.localStorage.time)/1000);
+			assets.friendliness.scaleX = increaseStat.convert((Date.now()-window.localStorage.time)/1000);
+			
+			if (assets.hp.scaleX == 0) {
+				zog("dead");
+				clearInterval(interval);
+			}
+			
+			stage.update();
+		}, 1000);
 	}
 	
 	return app;
--- www/js/view.js
+++ www/js/view.js
@@ -8,61 +8,117 @@ var app = function(app) {
 		p.main.name = "main";
 		p.main.setBounds(0, 0, stageW, stageH);
 		
-		/*var logo = p.logo = new zim.Rectangle(100, 100, "orange");
-		logo.setBounds(0, 0, 100, 100);
-		p.main.addChild(logo);*/
-		
 		var content = p.main.content = new createjs.Container();
 		content.setBounds(0, 0, 600, 600);
 		p.main.addChild(content);
 		
-		/*var thing = p.mainThing = new zim.Rectangle(200, 200, "red");
-		thing.x = 100;
-		thing.y = 100;
-		content.addChild(thing);*/
+		var room = p.room = new createjs.Container();
+		room.w = content.getBounds().width;
+		room.h = content.getBounds().height;
+		content.addChild(room);
 		
-		//action container for: food, water, train
-		var buttonW = 400, buttonH = 600;
+		var backing = new zim.Rectangle(room.w, room.h, "black");
+		backing.alpha = 0.2;
+		room.addChild(backing); 
 		
-		var actions = p.main.actions = new createjs.Container();
-		actions.setBounds(0, 0, 1200, 350);
-		p.main.addChild(actions);
+		var chara = p.chara = new zim.Rectangle(100, 100, "black");
+		chara.x = room.w/2-45;
+		chara.y = room.h/2-75;
+		chara.setBounds(0, 0, 100, 100);
+		room.addChild(chara);
+		
+		//button container for: food, water, train
+		var buttonW = 400, buttonH = 400;
+		var buttons = p.buttons = new createjs.Container();
+		buttons.setBounds(0, 0, 1200, 350);
+		p.main.addChild(buttons);
 		
 		var food = p.food = new zim.Rectangle(buttonW, buttonH, "green");
-		food.x = 0;
-		food.y = 0;
-		actions.addChild(food);
+		food.x = buttons.getBounds().x;
+		food.y = buttons.getBounds().y;
+		food.setBounds(0, 0, buttonW, buttonH);
+		buttons.addChild(food);
 		
 		var water = p.water = new zim.Rectangle(buttonW, buttonH, "blue");
-		water.x = buttonW;
-		water.y = 0;
-		actions.addChild(water);
+		water.x = buttons.getBounds().x + buttonW;
+		water.y = buttons.getBounds().y;
+		water.setBounds(0, 0, buttonW, buttonH);
+		buttons.addChild(water);
 		
 		var training = p.training = new zim.Rectangle(buttonW, buttonH, "red");
-		training.x = buttonW * 2;
-		training.y = 0;
-		actions.addChild(training);
+		training.x = buttons.getBounds().x + buttonW * 2;
+		training.y = buttons.getBounds().y;
+		training.setBounds(0, 0, buttonW, buttonH);
+		buttons.addChild(training);
 		
-		//gridManager.add(new zim.Grid(actions));
-		//guideManager.add(new zim.Guide(actions, false));
+		//gridManager.add(new zim.Grid(content));
+		//guideManager.add(new zim.Guide(__, false));
 		
 		//percentages
 		var mainParts = [
 			{object: content, marginTop: 0, maxWidth: stageW, backgroundColor: "beige"}, // note, middle gets no minHeight
-			{object: actions, marginTop: 0, maxWidth: stageW, height: 20, valign: "bottom", backgroundColor: "orange"}
+			{object: buttons, marginTop: 0, maxWidth: stageW, height: 20, valign: "bottom", backgroundColor: "orange"}
 		];
 		
 		//function(holder, regions, lastMargin, backgroundColor, vertical, regionShape, scalingObject, hideKey)
 		var mainLayout = new zim.Layout(p.main, mainParts, 0, "black", true, null, stage);
-		
 		layoutManager.add(mainLayout); //add main page to the layout manager
 		
 		p.status = new createjs.Container();
 		p.status.name = "status";
 		
-		var statusBacking = new zim.Rectangle(stageW, stageH, "yellow");
+		/*var statusBacking = new zim.Rectangle(stageW, stageH, "#aee");
 		statusBacking.setBounds(0, 0, stageW, stageH);
-		p.status.addChild(statusBacking);
+		p.status.addChild(statusBacking);*/
+		
+		var stats = new createjs.Container();
+		stats.setBounds(0, 0, 600, 1000);
+		p.status.addChild(stats);
+		
+		//create stat bars
+		var barContainer = new createjs.Container();
+		var w = stats.getBounds().width, h = 60, gap = 30;
+		var statBar = 5, barBacking;
+		
+		for (var i = 0; i < statBar; i++) {
+			barBacking = new zim.Rectangle(w, h, "gray");
+			barBacking.x = 0;
+			barBacking.y = i * (h + gap);
+			barContainer.addChild(barBacking);
+			stats.addChild(barContainer);
+		}
+		
+		var hp = p.hp =  new zim.Rectangle(w, h, "red");
+		hp.scaleX = 1;
+		hp.x = 0;
+		hp.y = 0;
+		stats.addChild(hp);		
+		
+		var hungerStat = p.hungerStat =  new zim.Rectangle(w, h, "green");
+		hungerStat.scaleX = 1;
+		hungerStat.x = 0;
+		hungerStat.y = h + gap;
+		stats.addChild(hungerStat);
+		
+		var waterStat = p.waterStat =  new zim.Rectangle(w, h, "blue");
+		waterStat.scaleX = 1;
+		waterStat.x = 0;
+		waterStat.y = (h + gap) * 2;
+		stats.addChild(waterStat);
+		
+		var friendliness = p.friendliness = new zim.Rectangle(w, h, "yellow");
+		friendliness.scaleX = 0;
+		friendliness.x = 0;
+		friendliness.y = (h + gap) * 3;
+		stats.addChild(friendliness);
+		
+		var statusParts = [
+			{object: stats, marginTop: 20, maxWidth: 90, valign: "middle"}
+		];
+		
+		//function(holder, regions, lastMargin, backgroundColor, vertical, regionShape, scalingObject, hideKey)
+		var statusLayout = new zim.Layout(p.status, statusParts, 0, "#aee", true, null, stage);
+		layoutManager.add(statusLayout); //add status page to the layout manager
 		
 		return p;
 	}
